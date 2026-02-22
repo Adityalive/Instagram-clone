212cd17deb433c6e61ad5187de280af4189f79dc
Aditya <adityakumar4176o@gmail.com>
2026-02-23 02:23:59 +0530

    refactor: wrap AppRoutes with AuthProvider for authentication context

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import AppRoutes from './Approutes'
+import { AuthProvider } from './features/auth/auth.context.jsx'
+
 const App = () => {
   return (
-<main>
-    <AppRoutes/>
- </main>  )
+    <AuthProvider>
+      <main>
+        <AppRoutes/>
+      </main>
+    </AuthProvider>
+  )
 }
 
 export default App
--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -1,11 +1,12 @@
-import { useState,createContext } from "react";
+import { useState, createContext } from "react";
 
 export const AuthContext = createContext();
-export async function AuthProvider({ children }) {
+
+export function AuthProvider({ children }) {
     const [User, setUser] = useState(null);
     const [Loading, setLoading] = useState(false);
     return (
-        <AuthContext.Provider value={{ User, setUser ,Loading, setLoading}}>
+        <AuthContext.Provider value={{ User, setUser, Loading, setLoading }}>
             {children}
         </AuthContext.Provider>
     );
